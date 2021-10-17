7a538606235a18e79c6518d437ff930056e6b867
AymericROUVRAIS <54468077+AymericROUVRAIS@users.noreply.github.com>
2021-10-17 21:24:25 +0200

    Add files via upload

---
+++ env.py
@@ -0,0 +1,18 @@
+# libraries :
+from discord.ext import commands
+import discord, asyncio, random
+from discord.utils import get
+from discord_components import DiscordComponents, Button, ButtonStyle, component
+
+
+
+# variables :
+TOKEN = 'ODQwMjcwNDUxOTAxMDcxNDIw.YJVw1w.rV8xEjQIBFsf1bGJ0lAKEn2-7Pk'
+guild = '840268645996429343'
+roles = [
+    ('werewolves', 1),
+    ('witch', 0),
+    ('seer', 0),
+    ('cupid', 0),
+    ('hunter', 0)
+    ] # 0 : only 1 player will have the role, 1 : 'indefinite'
