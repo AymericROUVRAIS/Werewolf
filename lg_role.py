71c92d7251455dc2ceb332d2e48d41696f96ff85
AymericROUVRAIS <54468077+AymericROUVRAIS@users.noreply.github.com>
2021-10-17 21:26:04 +0200

    Add files via upload

---
+++ lg_role.py
@@ -0,0 +1,27 @@
+class loupGarou:
+    def __init__(self):
+        pass
+
+    def vote(self):
+        pass
+
+    def __str__(self):
+        return ''
+
+class voyante:
+    pass
+
+class chasseur:
+    pass
+
+class sorciere:
+    pass
+
+class cupidon:
+    pass
+
+class voleur:
+    pass
+
+class petiteFille:
+    pass
